93bd9390b8bfc7263c306db23747e2c0148ddcf7
Wung Brandon <wungbrandon27@gmail.com>
2024-10-12 14:11:05 +0100

    added women products and its images at the djangpo admin panel.

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -5,7 +5,11 @@ from .models import (
     SubCategory, 
     Brand, 
     Product,
-    Review
+    Review,
+    Cart,
+    CartItem,
+    Wishlist,
+    WishlistItem
 )
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -50,7 +54,7 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'name', 'description', 'brand', 'category', 'subcategory', 'price', 'stock', 
                   'image', 'video', 'is_featured', 'is_new_arrival', 'slug', 
-                  'images', 'additional_images', 'review_count', 'average_rating']
+                  'images', 'additional_images', 'review_count', 'average_rating', 'sales_count']
         read_only_fields = ['id', 'is_featured', 'is_new_arrival']
 
     def create(self, validated_data):
@@ -85,45 +89,109 @@ class ProductSerializer(serializers.ModelSerializer):
     #     return RelatedProductSerializer(related_products, many=True).data
 
 
-# Create a smaller serializer for related products to avoid recursion
-class RelatedProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True, read_only=True)
-    class Meta:
-        model = Product
-        fields = ['id', 'name', 'price', 'slug', 'image', 'images', 'description', 'category', 'subcategory', 'stock', 'brand']  # Only include essential fields
+# # Create a smaller serializer for related products to avoid recursion
+# class RelatedProductSerializer(serializers.ModelSerializer):
+#     images = ProductImageSerializer(many=True, read_only=True)
+#     class Meta:
+#         model = Product
+#         fields = ['id', 'name', 'price', 'slug', 'image', 'images', 'description', 'category', 'subcategory', 
+#                     'stock', 'brand', 'review_count', 'average_rating', 'sales_count']  # Only include essential fields
     
 
-# Serializer for Featured Products
-class FeaturedProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True, read_only=True)
+# # Serializer for Featured Products
+# class FeaturedProductSerializer(serializers.ModelSerializer):
+#     images = ProductImageSerializer(many=True, read_only=True)
     
-    class Meta:
-        model = Product
-        fields = ['id', 'name', 'price', 'image', 'slug', 'description', 'category', 'subcategory', 'stock', 'brand', 'images']
-        read_only_fields = ['id']
+#     class Meta:
+#         model = Product
+#         fields = ['id', 'name', 'price', 'image', 'slug', 'description', 'category', 'subcategory', 'stock', 
+#                   'brand', 'images', 'review_count', 'average_rating', 'sales_count']
+#         read_only_fields = ['id']
 
-# Serializer for New Arrivals
-class NewArrivalProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True, read_only=True)
-    class Meta:
-        model = Product
-        fields = ['id', 'name', 'price', 'image', 'slug', 'description', 'category', 'subcategory', 'stock', 'brand', 'images']
-        read_only_fields = ['id']
+# # Serializer for New Arrivals
+# class NewArrivalProductSerializer(serializers.ModelSerializer):
+#     images = ProductImageSerializer(many=True, read_only=True)
+#     class Meta:
+#         model = Product
+#         fields = ['id', 'name', 'price', 'image', 'slug', 'description', 'category', 'subcategory', 'stock', 
+#                   'brand', 'images', 'review_count', 'average_rating', 'sales_count']
+#         read_only_fields = ['id']
 
-# Serializer for Recent Products
-class RecentProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True, read_only=True)
-    class Meta:
-        model = Product
-        fields = ['id', 'name', 'price', 'image', 'slug', 'description', 'category', 'subcategory', 'stock', 'brand', 'images']
-        read_only_fields = ['id']
+# # Serializer for Recent Products
+# class RecentProductSerializer(serializers.ModelSerializer):
+#     images = ProductImageSerializer(many=True, read_only=True)
+#     class Meta:
+#         model = Product
+#         fields = ['id', 'name', 'price', 'image', 'slug', 'description', 'category', 'subcategory', 'stock', 
+#                   'brand', 'images', 'review_count', 'average_rating', 'sales_count']
+#         read_only_fields = ['id']
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True)  # Display user's username
     product = serializers.StringRelatedField(read_only=True)  # Display product's name
 
     class Meta:
         model = Review
-        fields = ['id', 'product', 'user', 'rating', 'review']
-        read_only_fields = ['id', 'user', 'product']
+        fields = ['id', 'product', 'username', 'rating', 'review']
+        read_only_fields = ['id', 'username', 'product']
+    
+class CartItemSerializer(serializers.ModelSerializer):
+    product = serializers.StringRelatedField()
+    cart = serializers.StringRelatedField()
+    total_price = serializers.SerializerMethodField(read_only=True) # Display
+    unit_price = serializers.SerializerMethodField(read_only=True)  # Display unit price
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'cart', 'unit_price', 'quantity', 'total_price']
+        read_only_fields = ['id']
+        
+    def total_price(self, obj):
+        """Calculate total price for this cart item based on the quantity"""
+        return obj.total_price()
+        
+    def get_unit_price(self, obj):
+        """Retrieve the unit price of the product"""
+        return obj.product.price
+
+class CartSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField() # Display
+    total_price = serializers.SerializerMethodField(read_only=True) # Display
+    total_items = serializers.SerializerMethodField(read_only=True) # Display
+    items = CartItemSerializer(many=True)  # Use the related_name
+    items_count = serializers.SerializerMethodField(read_only=True) #
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'total_price', 'total_items', 'items', 'items_count']
+        read_only_fields = ['id']
+        
+    def get_total_price(self, obj):
+        """Calculate total price of all items in the cart."""
+        return obj.total_price
+    def get_total_items(self, obj):
+        """Get total count of items in the cart."""
+        return obj.total_items
+    
+    def get_items_count(self, obj):
+        return obj.total_items_count
+    
+class WishlistItemSerializer(serializers.ModelSerializer):
+    product = serializers.StringRelatedField()  # Display product name
+
+    class Meta:
+        model = WishlistItem
+        fields = ['id', 'product', 'wishlist']  # Include any other fields you need
+        read_only_fields = ['id']
+        
+class WishlistSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()  # Display user
+    total_items = serializers.SerializerMethodField(read_only=True)  # Display total items
+    items = WishlistItemSerializer(source='wishlist_items', many=True)  # Use the related_name
+
+    class Meta:
+        model = Wishlist
+        fields = ['id', 'user', 'total_items', 'items']
+        read_only_fields = ['id']
+
+    def get_total_items(self, obj):
+        """Get total count of items in the wishlist."""
+        return obj.total_items  # Calls the property in Wishlist
